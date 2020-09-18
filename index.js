d683a61893a21aeb527418dba6f4e98e4402c536
Chad-Daya <zeac144@live.rhul.ac.uk>
2020-09-18 14:40:54 +0100

    Added Dice Image JS

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1 - 6
 var randomDiceImage = "dice" + randomNumber1 + ".png"; 
 
 var randomImageSource = "images/" + randomDiceImage;
+
+document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
